b6de31cbc2527b1e8f9aba4c3174d62bb09306a6
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-03-08 18:56:34 -0500

    person

---
+++ React/prop-it-up/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard last_name="Doe" first_name="Jane" age={45} haircolor="Black"/>
+      <PersonCard last_name="Smith" first_name="John" age={88} haircolor="Brown"/>
+      <PersonCard last_name="Fillmore" first_name="Millard" age={50} haircolor="Brown"/>
+      <PersonCard last_name="Smith" first_name="Maria" age={62} haircolor="Brown"/>
+    </div>
+  );
+}
+
+export default App;
