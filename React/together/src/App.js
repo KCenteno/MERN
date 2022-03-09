600bb61a91cb0027908178b53f581281e22a1748
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-03-08 20:35:18 -0500

    together

---
+++ React/together/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard last_name="Doe" first_name="Jane" age={45} haircolor="Black"/>
+      <PersonCard last_name="Smith" first_name="John" age={88} haircolor="Brown"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/together/src/components/PersonCard.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+
+class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age : props.age
+        }
+    }
+
+    updateState = () => {
+            this.setState({
+                age: this.state.age + 1})
+    }
+
+    render(){
+        const{first_name, last_name, haircolor} = this.props;
+        return(
+            <div>
+                <h1>{last_name}, {first_name}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {haircolor}</p>
+                <button onClick={this.updateState}>Birthday Button for {first_name} {last_name}</button>
+            </div>
+        )
+    }
+}
+
+
+export default PersonCard;
