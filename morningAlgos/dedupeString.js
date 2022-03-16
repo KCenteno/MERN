dec681312135ecf594d4c2c7ec62212d7e7bced8
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-03-16 11:13:59 -0400

    commit

--- morningAlgos/dedupeString.js
+++ morningAlgos/dedupeString.js
@@ -3,25 +3,43 @@
 //and don't you dare use built in functions for this one!
 //"Snaps! crackles! pops!" -> "Snrackle ops!"
 //scoop dedupe!
+
+// const dedupe = (str) => {
+//     var newStr=""
+//     var inStr=false
+//     for (i=str.length-1; i >= 0; i--){
+//         inStr=false;
+//         if(newStr===""){
+//             newStr= str[i]
+//         }
+//         for(j=0; j<newStr.length-1; j++){
+//             if(newStr[j]===str[i]){
+//                 inStr=true;
+//                 break;
+//             }
+//         }
+//         if (!inStr){
+//         newStr= str[i] + newStr
+//         }
+//     }
+//     return newStr
+// }
+
 const dedupe = (str) => {
-    var newStr=""
-    var inStr=false
-    for (i=str.length-1; i >= 0; i--){
-        inStr=false;
-        if(newStr===""){
-            newStr= str[i]
+    const letters = {};
+    let newStr = "";
+    for(let i = str.length - 1; i >= 0; i--){
+        if(!letters[str[i]]){
+            console.log(`found new letter: ${str[i]}`);
+            newStr = str[i] + newStr;
+            letters[str[i]] = true;
         }
-        for(j=0; j<newStr.length-1; j++){
-            if(newStr[j]===str[i]){
-                inStr=true;
-                break;
-            }
-        }
-        if (!inStr){
-        newStr= str[i] + newStr
+        else{
+            console.log(`found old letter: ${str[i]}`);
         }
     }
-    return newStr
+    console.log(letters);
+    return newStr;
 }
 
 console.log(dedupe("Snaps! crackles! pops!"));
